3d6ebf2bed2fa58615736f7cf258d90a0a503a29
Gaurav Gokhale <gauravgokhale752@gmail.com>
2024-04-07 22:27:05 +0200

    added none-ls, not working

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,17 +1,17 @@
 return {
   {
-     "williamboman/mason.nvim",
-      config = function()
-        require("mason").setup()
-      end
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
   },
   {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "clangd" }
+        ensure_installed = { "lua_ls", "clangd" },
       })
-    end
+    end,
   },
   {
     "neovim/nvim-lspconfig",
@@ -19,10 +19,9 @@ return {
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
       lspconfig.clangd.setup({})
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
-    end
-
-  }
- }
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
+}
