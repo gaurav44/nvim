c46beaaf33b5f02e44a249072672a21f3df97bb6
Gaurav Gokhale <gauravgokhale752@gmail.com>
2024-07-16 21:17:53 +0200

    terminal added

--- lua/plugins/tabline.lua
+++ lua/plugins/tabline.lua
@@ -4,7 +4,7 @@ return {
     'lewis6991/gitsigns.nvim',
     'nvim-tree/nvim-web-devicons',
   },
- 
+
   init = function() vim.g.barbar_auto_setup = false end,
   config = function()
     require('barbar').setup({
