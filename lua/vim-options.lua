f64f7dbf7438711b9d8b897ecd7f812e9e83a2f4
Gaurav Gokhale <gauravgokhale752@gmail.com>
2024-10-09 17:45:27 +0200

    code folding, leetcode and markdown

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,3 +4,9 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set relativenumber")
 vim.g.mapleader = " "
+vim.o.foldmethod = 'syntax'
+vim.o.foldlevelstart = 99
+vim.cmd('filetype plugin indent on')
+vim.api.nvim_set_keymap('n', '<space>', 'za', { noremap = true, silent = true })  -- Toggle fold
+vim.api.nvim_set_keymap('n', 'zc', 'zM', { noremap = true, silent = true })       -- Close all folds
+vim.api.nvim_set_keymap('n', 'zo', 'zR', { noremap = true, silent = true })       -- Open all folds
